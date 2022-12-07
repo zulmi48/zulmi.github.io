15d6cad3af69415006fd357598a8eb863f0b1116
Zulmi Fuada <zulmi.f48@gmail.com>
2022-12-07 10:39:14 +0700

    Add minor javascript changes

--- js/script.js
+++ js/script.js
@@ -3,12 +3,27 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 100);
 });
 
+const navlist = document.querySelector(".navlist");
+navlist.addEventListener("click", function (e) {
+  const target = e.target;
+  if (target.classList.contains("menu__link")) {
+    const linkActive = document.querySelector("ul li a.active");
+    if (
+      linkActive !== null &&
+      target.getAttribute("href") !== linkActive.getAttribute("href")
+    ) {
+      linkActive.classList.remove("active");
+    }
+    target.classList.add("active");
+  }
+});
+
 let menu = document.querySelector("#menu-icon");
-let navlist = document.querySelector(".navlist");
+let navlistMobile = document.querySelector(".navlist");
 
 menu.onclick = () => {
   menu.classList.toggle("bx-x");
-  navlist.classList.toggle("open");
+  navlistMobile.classList.toggle("open");
 };
 
 window.onscroll = () => {
